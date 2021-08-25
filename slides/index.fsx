ee55e8ea2da4213ccf0fc3d0647c708c63363664
zhangweijun <ahnqihyun@sina.cn>
2021-08-25 14:20:39 +0800

    update index page

---
+++ slides/index.fsx
@@ -0,0 +1,111 @@
+(**
+- title : FsReveal
+- description : Introduction to FsReveal
+- author : Karlkim Suwanmongkol
+- theme : sky
+- transition : default
+
+***
+### Who am I?
+
+- 工作年限: 4年
+- 前端开发 [React](https://reactjs.org/)
+- like program such as [F#](https://fsharp.org/), [OCaml](https://ocaml.org/)
+- homepage [github](https://github.com/Zhang-WJ)
+
+***
+
+### 项目经验
+
+#### 绵阳智慧鸟 微信小程序
+
+>   - Taro + Taro UI组件库
+>   - 主要功能为移动售货柜查询，商品展示，购物车，订单详情
+>   - 主要负责整个项目的前端部分
+
+---
+
+#### 绵阳智慧鸟 管理平台
+
+>   - Angular 7.0 + NG-ZORRO组件库
+>   - 主要功能为移动售货柜的收益管理，货柜格口管理，支付账号管理，平台货物管理
+>   - 主要负责整个项目的前端部分
+
+---
+
+#### Teammark
+
+>   - React
+>   - 主要功能：项目空间、在线文档、在线表格、网盘等应用
+>   - 主要负责整个项目的前端部分
+
+---
+
+#### 约吗-App
+
+>   - React
+>   - 主要功能：首页，大厅聊天，交友，充值服务等
+>   - 主要负责项目的前端页面,PC活动页面等
+>   - H5活动页面等
+
+---
+
+#### TMMTMM-App
+
+>   - React + Electron
+>   - 主要功能：聊天（文件，图片等），好友添加等
+>   - 主要负责项目主要功能实现。
+
+
+***
+- data-background : images/fsharp128.png
+- data-background-repeat : repeat
+- data-background-size : 100px
+
+### 业余喜欢函数式语言（F#, OCaml）
+
+#### F#
+
+*)
+let a = 5
+let factorial x = [1..x] |> List.reduce (*)
+let c = factorial a
+(**
+`c` is evaluated for you
+*)
+(*** include-value: c ***)
+(**
+
+---
+
+#### 更多 F#
+
+*)
+[<Measure>] type sqft
+[<Measure>] type dollar
+let sizes = [|1700<sqft>;2100<sqft>;1900<sqft>;1300<sqft>|]
+let prices = [|53000<dollar>;44000<dollar>;59000<dollar>;82000<dollar>|]
+(**
+
+#### `prices.[0]/sizes.[0]`
+
+*)
+(*** include-value: prices.[0]/sizes.[0] ***)
+(**
+
+***
+
+###  程序员的真实生活
+
+**When I show my boss that I've fixed a bug:**
+
+![When I show my boss that I've fixed a bug](http://www.topito.com/wp-content/uploads/2013/01/code-07.gif)
+
+**When your regular expression returns what you expect:**
+
+![When your regular expression returns what you expect](http://www.topito.com/wp-content/uploads/2013/01/code-03.gif)
+
+*from [The Reality of a Developer's Life - in GIFs, Of Course](http://server.dzone.com/articles/reality-developers-life-gifs)*
+
+***
+*)
